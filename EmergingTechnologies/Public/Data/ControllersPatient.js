8b3ca53c35bac6bbe5d5971cdd9155457d26fcc3
srichatala <sri2vas@gmail.com>
2015-03-27 19:34:25 -0400

    MEAN stack application with passportjs

--- EmergingTechnologies/Public/Data/ControllersPatient.js
+++
@@ -1,1 +0,0 @@
-﻿var appDoc = angular.module('ControllersPatient', []);
---
+++ EmergingTechnologies/Public/app/controllers/DocProfileCtrl.js
@@ -0,0 +1,7 @@
+﻿app.controller('DocProfileCtrl', function ($scope, $http) {
+
+    $http.get("/rest/user")
+    .success(function (users) {
+        $scope.users = users;
+    });
+});
